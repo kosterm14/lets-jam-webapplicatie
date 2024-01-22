b2202913e06b5c33b0a68cbc47bc743f1adffff9
Marwaxhello <marwahello33@gmail.com>
2024-01-22 16:54:35 +0100

    push

--- src/lib/organisms/kennisclips.svelte
+++ src/lib/organisms/kennisclips.svelte
@@ -22,7 +22,7 @@
       <iframe
         class="frame"
         width="560"
-        height="314"
+        height="315"
         src={category.youTubeLink}
         frameborder="0"
         allowfullscreen
